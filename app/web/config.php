0bb842cc0ba89bae8d46e089335305e500a117b5
Archie Alvarado <134375640+ArchieAlvarado20@users.noreply.github.com>
2025-09-05 14:43:33 +0800

    Update config.php

--- app/web/config.php
+++ app/web/config.php
@@ -12,11 +12,6 @@ define('DB_DRIVER', 'mysql');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
 
-	// define('DB_HOST', 'sql105.infinityfree.com');
-	// define('DB_NAME', 'if0_39719926_dotsbrew_db');
-	// define('DB_USER', 'if0_39719926');
-	// define('DB_PASS', 'Ellengay1');
-
 /**unique key for this project**/
 define('UNIQUE', 'xdght');
 
